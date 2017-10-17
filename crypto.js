6fbda10f93659616899cba04b0dc0eff74c13cdd
Adrian <dbarra@agetic.gob.bo>
2017-10-17 18:02:28 -0400

    init crypto

---
+++ crypto.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.addEventListener('DomContentLoaded', () => {
+	if (!window.crypto && !window.crypto.subtle) {
+		console.warn('Su navegador no soporta webCrypto');
+	} else {
+		
+	}
+})
