7f6f8621f0f745b61373e2717e08da4e5c3d2555
Adrian <dbarra@agetic.gob.bo>
2017-10-17 18:22:19 -0400

    new changes

--- crypto.js
+++ crypto.js
@@ -1,9 +1,35 @@
 'use strict';
-
-document.addEventListener('DomContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 	if (!window.crypto && !window.crypto.subtle) {
-		console.warn('Su navegador no soporta webCrypto');
+		console.warn('Your current browset not support webCrypto Api');
 	} else {
-		
+		let keyPair = null;
+		createAndSaveKeyPair()
+			.then((res) => {
+				keyPair = res;
+				console.log('-----------keyPair-------------------------');
+				console.log(keyPair);
+				console.log('------------------------------------');
+				document.querySelector('#encrypt').addEventListener('click', 'encrypt');
+				document.querySelector('#decrypt').addEventListener('click', 'decrypt');
+			})
+			.catch((err) => {
+				console.error(err.message);
+			})
 	}
-})
+});
+
+const createAndSaveKeyPair = () => {
+	return window.crypto.subtle.generateKey({
+				name: 'RSA-OAEP',
+				modulusLength: 2048,
+				publicExponent: new Uint8Array([1, 0, 1]),
+				hash: {
+					name: "SHA-256"
+				}
+			},
+			true, ["encrypt", "decrypt"])
+		.then((res) => {
+			return res;
+		})
+}
